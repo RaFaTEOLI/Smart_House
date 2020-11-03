47ee4d1a50a992ef9e3774d079264d454c666180
Rafael Tessarolo <rafinha.tessarolo@hotmail.com>
2020-11-03 11:35:36 -0300

    Corrigido log de ativação

--- pages/aparelhos/ativar_aparelho.php
+++ pages/aparelhos/ativar_aparelho.php
@@ -1,9 +1,10 @@
 <?php
 require_once("../../root.php");
+require_once($root . "/includes/parametros.php");
+require_once($root . "/includes/conexao/conn.php");
 
 include($root . "/dao/DaoAparelho.php");
 include($root . "/dao/DaoAtivacao.php");
-require_once($root . "/includes/conexao/conn.php");
 
 $daoAparelho = new DaoAparelho();
 $daoAtivacao = new DaoAtivacao();
