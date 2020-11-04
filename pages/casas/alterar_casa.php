f7ae451beacfb3e495057bc2392a3b0be943a3d6
Rafael Tessarolo <rafinha.tessarolo@hotmail.com>
2020-11-04 09:44:17 -0300

    File upload fix

--- pages/casas/alterar_casa.php
+++ pages/casas/alterar_casa.php
@@ -74,7 +74,7 @@
       // if everything is ok, try to upload file
       } else {
           if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-              $caminho_foto = str_replace('../../', '/smart_house/', $target_file);
+              $caminho_foto = str_replace('../../', '', $target_file);
               echo "The file ". $basename_file . " has been uploaded on " . $caminho_foto;
 
               if($daoCasa->alterarCasa($conn, $_POST, $caminho_foto)) {
